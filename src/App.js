cab31fb1d8e5e8b7162e66104077be91e50637f0
Dmitry <eralashpvp@gmail.com>
2020-06-26 00:03:30 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Hello working</h2>
+        <p>Lorem ipsum loren</p>
+        <div className="prog-start">
+            <p>Some times we have problems</p>
+            <p>And some  time we have some interesting</p>
+            <p>but pizza was always been</p>          
+        </div>
     </div>
   );
 }
