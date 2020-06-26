1e0f62e17d8b59c8786c43dc4c3bb551fecfaf20
Dmitry <eralashpvp@gmail.com>
2020-06-26 22:27:49 +0300

    now we have router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
+
+
+import Header from './header';
+import Footer from './footer';
+
 function App() {
   return (
-    <div className="App">
-        Heeey
+    <div className="Main">
+        <Router>
+        <Header/>
+
+
+
+
+
+        <Footer/>
+        </Router>
     </div>
   );
 }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = ( ) =>{
+    return(
+        <div>TIS FOOTER TIME</div>
+    )
+}
+
+export default Footer;
