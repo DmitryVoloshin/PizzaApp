ecf785f31e120e2c95c8f456d34e34821befb70d
Dmitry <eralashpvp@gmail.com>
2020-06-27 22:29:13 +0300

    some html + css

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -8,10 +8,23 @@ const MainPage = ( ) =>{
     return (
         <main className="main-page_block">
                 <div className="circle-pizza_pice"></div>
+                <div className="right-block_pizza">
+                <h2 className="right-block_title">Italian Pizza</h2>
+                <p className="right-block_about">
+                A small 
+                river named Duden flows
+                by their place and supplies it 
+                with the necessary regelialia
+                </p>
+                <div className="pizza-block_buttons">
+                <Link to="/constructor" className="pizza-constructor_block">
+                    <button className="pizza-constructor_button view-menu_button">View Menu</button>
+                </Link>
                 <Link to="/constructor" className="pizza-constructor_block">
                     <button className="pizza-constructor_button">I want my Pizza</button>
                 </Link>
-             
+                </div>
+                </div>
         </main>
     )
 }
---
+++ src/components/pages/reused-blocks/service-block/service-block.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ServiceBlock = ( ) =>{
+    return(
+        <div>Service block</div>
+    )
+}
+
+export default ServiceBlock
