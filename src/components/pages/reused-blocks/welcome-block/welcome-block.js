f06c4154f27c8d3b67c99357a97740dc518e70c5
Dmitry <eralashpvp@gmail.com>
2020-06-28 11:17:28 +0300

    some changes

--- src/components/pages/reused-blocks/welcome-block/welcome-block.js
+++ src/components/pages/reused-blocks/welcome-block/welcome-block.js
@@ -5,11 +5,21 @@ import './welcome-block.css'
 const WelocomeBlock = ( ) =>{
     return(
         <div className="main-page_welcome">
-            <div className="welcome-right_block">
+            <div className="welcome-left_block">
                 1
             </div>
-            <div className="welcome-left_block">
-                2
+            <div className="welcome-right_block">
+                <h2 className="welcome-right_title">Welcome</h2>
+                <p className="welcome-right_text">
+                    On her way she met a copy. 
+                The copy warned the Little Blind Text, that where it came from 
+                it would have been rewritten a thousand times and everything that
+                 was left from its origin would be the word "and" and the Little 
+                 Blind Text should turn around and return to its own, safe country. 
+                 But nothing the copy said could convince her and so it didn’t take long 
+                 until a few insidious Copy Writers ambushed her, made her drunk with 
+                 Longe and Parole and dragged her into their agency, where they abused her for their.
+                 </p>
             </div>
         </div>
     )
