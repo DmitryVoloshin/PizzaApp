8fb7e695391a9393b0ba98b75e34bc39d9369824
Dmitry <eralashpvp@gmail.com>
2020-06-28 19:52:44 +0300

    some

--- src/components/pages/reused-blocks/service-block/service-block.js
+++ src/components/pages/reused-blocks/service-block/service-block.js
@@ -4,11 +4,15 @@ import './service-block.css'
 
 import vegetable from './../../../../img/icons/service-icons/vegetable.png';
 import delivery from './../../../../img/icons/service-icons/delivery.png';
+import slice from './../../../../img/icons/service-icons/slice.png'
 
 const ServiceBlock = ( ) =>{
+
+
     return(
+    
         <div className="service-block">
-            <h2 className="service-block_header">OUR SERVICES</h2>
+            <h3 className="service-block_header">OUR SERVICES</h3>
             <p className="service-block_text">Far far away, behind the word mountains, 
             far from the countries Vokalia and Consonantia, there live the blind texts.
             </p>
@@ -18,31 +22,35 @@ const ServiceBlock = ( ) =>{
                     <div className="service-item_image">
                        <img src={vegetable} className="item-image"/>
                     </div>
-                    <h2 className="service-item_title"></h2>
+                    <h3 className="service-item_title">HEALTHY FOODS</h3>
                     <p className="service-item_text">
-
+                    Even the all-powerful Pointing has no control about 
+                    the blind texts it is an almost unorthographic.
                     </p>
                 </li>
                 <li className="service-item">
                     <div className="service-item_image">
                         <img src={delivery} className="item-image"/>
                     </div>
-                    <h2 className="service-item_title"></h2>
+                    <h3 className="service-item_title">FASTEST DELIVERY</h3>
                     <p className="service-item_text">
-
+                    Even the all-powerful Pointing has no control about 
+                    the blind texts it is an almost unorthographic.
                     </p>
                 </li>
                 <li className="service-item">
                     <div className="service-item_image">
-                        3
+                        <img src={slice} className="item-image"/>
                     </div>
-                    <h2 className="service-item_title"></h2>
+                    <h3 className="service-item_title">ORIGINAL RECIPES</h3>
                     <p className="service-item_text">
-
+                    Even the all-powerful Pointing has no control 
+                    about the blind texts it is an almost unorthographic.
                     </p>
                 </li>
             </ul>
         </div>
+ 
     )
 }
 
