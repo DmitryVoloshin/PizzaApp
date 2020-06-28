a18a516f2686440442b1ee81e320abe13a95ac24
Dmitry <eralashpvp@gmail.com>
2020-06-28 23:25:26 +0300

    last today

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './footer.css'
 
 import image1 from './../../img/footerimg/image1.jpg'
+import image2 from './../../img/footerimg/image2.jpg'
 
 const Footer = ( ) =>{
     return(
@@ -15,18 +16,22 @@ const Footer = ( ) =>{
                             far from the countries Vokalia and Consonantia, 
                             there live the blind texts.</p>
                     </li>
-                    <li className="footer-block_item">
+                    <li className="footer-block_item blog-width">
                         <h2 className="footer-item_title">RECENT BLOG</h2>
                         <div className="blog-item">
                             <div className="blog-item_block">
                                 <img src={image1} className="footer-blog_image"/>
                                 <div className="blog-item_first">
                                     <a>Even the all-powerful Pointing has no control about</a>
-                                    <span>1</span>
+                                    <span className="blog-item_text">
+                                        <p>2</p>
+                                        <p>3</p>
+                                        <p>4</p>
+                                    </span>
                                 </div>
                             </div>
                             <div className="blog-item_block">
-                                <img src={image1} className="footer-blog_image"/>
+                                <img src={image2} className="footer-blog_image"/>
                                 <div className="blog-item_first">
                                     <a>Even the all-powerful Pointing has no control about</a>
                                     <span>1</span>
