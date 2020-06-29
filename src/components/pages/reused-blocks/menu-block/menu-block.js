166b5d203eebbb429c013a8c6424c3606f0584a8
Dmitry <eralashpvp@gmail.com>
2020-06-29 14:06:34 +0300

    menu block

--- src/components/pages/reused-blocks/menu-block/menu-block.js
+++ src/components/pages/reused-blocks/menu-block/menu-block.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './menu-block.css'
 
+import menu from './../../../../img/time-plug/menu.png';
 
 const MenuBlock = ( ) =>{
 
@@ -14,12 +15,18 @@ const MenuBlock = ( ) =>{
                 <h2 className="menu-block_title">HOT PIZZA MEALS</h2>
                 <p className="menu-block_text">Far far away, behind the word mountains, 
                 far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <div className>
-
+                <div className="menu-block_list">   
+                    <img src={menu} className="plug-for_time"></img>
                 </div>
            </div>
-           <div>
-
+           <div className="menu-block_pricing">
+                <h2 className="pricing-block_title">Our Menu pricing</h2>
+                <div className="pricing-block_image"></div>
+                <p className="pricing-blcok_text">
+                Far far away, behind the word mountains, 
+                far from the countries Vokalia and Consonantia, 
+                there live the blind texts.
+                </p>
            </div>
         </div>
     )
