a08681b404e3c2b1e7793ca96a579bae51f1d304
Dmitry <eralashpvp@gmail.com>
2020-06-30 12:32:46 +0300

    rechange foto-block

--- src/components/pages/reused-blocks/foto-block/foto-block.js
+++ src/components/pages/reused-blocks/foto-block/foto-block.js
@@ -2,10 +2,34 @@ import React from 'react';
 
 import './foto-block.css'
 
+
 const FotoBlock = ( ) =>{
     return(
-        <section className="foto-block">
-            1
+        <section className="foto-block"> 
+           
+            <ul className="foto-block_list">  
+                <li className="foto-list_item">
+                    <span className="list-item_sqr">
+                        <img className="item-sqr_image"></img>
+                    </span>
+                </li>
+                <li className="foto-list_item">
+                    <span className="list-item_sqr">
+                        <img className="item-sqr_image"></img>
+                    </span>
+                </li>
+                <li className="foto-list_item">
+                    <span className="list-item_sqr">
+                        <img className="item-sqr_image"></img>
+                    </span>
+                </li>
+                <li className="foto-list_item">
+                    <span className="list-item_sqr">
+                        <img className="item-sqr_image"></img>
+                    </span>
+                </li>
+            </ul>
+
         </section>
     )
 }
