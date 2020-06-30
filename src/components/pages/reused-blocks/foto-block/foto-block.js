a42d7799d10eeca9da217997d95dad2f6a17fa58
Dmitry <eralashpvp@gmail.com>
2020-06-30 12:59:08 +0300

    reroll

--- src/components/pages/reused-blocks/foto-block/foto-block.js
+++ src/components/pages/reused-blocks/foto-block/foto-block.js
@@ -2,30 +2,31 @@ import React from 'react';
 
 import './foto-block.css'
 
+import search from './../../../../img/foto-block-images/search.png'
 
 const FotoBlock = ( ) =>{
     return(
         <section className="foto-block"> 
-           
+
             <ul className="foto-block_list">  
                 <li className="foto-list_item">
                     <span className="list-item_sqr">
-                        <img className="item-sqr_image"></img>
+                        <img  src={search} className="item-sqr_image"/>
                     </span>
                 </li>
                 <li className="foto-list_item">
                     <span className="list-item_sqr">
-                        <img className="item-sqr_image"></img>
+                    <img  src={search} className="item-sqr_image"/>
                     </span>
                 </li>
                 <li className="foto-list_item">
                     <span className="list-item_sqr">
-                        <img className="item-sqr_image"></img>
+                    <img  src={search} className="item-sqr_image"/>
                     </span>
                 </li>
                 <li className="foto-list_item">
                     <span className="list-item_sqr">
-                        <img className="item-sqr_image"></img>
+                    <img  src={search} className="item-sqr_image"/>
                     </span>
                 </li>
             </ul>
