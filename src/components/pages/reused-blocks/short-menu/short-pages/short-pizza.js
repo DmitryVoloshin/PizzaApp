c7492a4e7c4c0ecccedc9130ecb692028f178425
Dmitry <eralashpvp@gmail.com>
2020-07-01 15:22:25 +0300

    images

--- src/components/pages/reused-blocks/short-menu/short-pages/short-pizza.js
+++ src/components/pages/reused-blocks/short-menu/short-pages/short-pizza.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
-import './pages.css'
+import './pages.css';
+
+import pizza1 from './../../../../../img/short-menu/pizza1.jpg'
+import pizza2 from './../../../../../img/short-menu/pizza2.jpg'
+import pizza3 from './../../../../../img/short-menu/pizza3.jpg'
 
 const ShortPizza = ( ) =>{
     return(
-        <div className="short-block_page">Pizza</div>
+        <div className="short-block">
+        <div className="short-block_page">
+            <img className="short-menu_image" src={pizza1}/>
+            <h2 className="short-menu_title">Italian Pizza</h2>
+            <p className="short-menu_text">Far far away, behind the word mountains, 
+            far from the countries Vokalia and Consonantia.
+            </p>
+            <p className="short-menu_price"></p>
+            <button className="short-menu_tocart">Add to cart</button>
+        </div>
+        <div className="short-block_page">
+            <img className="short-menu_image" src={pizza2}/>
+            <h2 className="short-menu_title">Italian Pizza</h2>
+            <p className="short-menu_text">Far far away, behind the word mountains, 
+            far from the countries Vokalia and Consonantia.
+            </p>
+            <p className="short-menu_price"></p>
+            <button className="short-menu_tocart">Add to cart</button>
+        </div>
+        <div className="short-block_page">
+            <img className="short-menu_image" src={pizza3}/>
+            <h2 className="short-menu_title">Italian Pizza</h2>
+            <p className="short-menu_text">Far far away, behind the word mountains, 
+            far from the countries Vokalia and Consonantia.
+            </p>
+            <p className="short-menu_price"></p>
+            <button className="short-menu_tocart">Add to cart</button>
+        </div>
+        </div>
     )
 }
 
